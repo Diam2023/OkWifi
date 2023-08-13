05c0f64e621a8fb177b89994d2954ec81b077eb6
monoliths <monoliths-uni@outlook.com>
2023-08-13 17:16:50 +0800

    OkWifi Update

--- main/main.cc
+++ main/main.cc
@@ -8,73 +8,144 @@
 #include <memory>
 #include <thread>
 
+#include <nvs_flash.h>
+#include <wifi_provisioning/scheme_ble.h>
+#include <esp_wifi_default.h>
+#include <esp_wifi.h>
+
 #include "esp_timer_cxx.hpp"
 #include "esp_event_cxx.hpp"
 #include "gpio_cxx.hpp"
 
+#include "wifi_provisioning/manager.h"
+
 // ms s us
 using namespace std::chrono_literals;
 
-//{
-//
-//using namespace idf::event;
-//
-//// Create A Timer For Application
-//auto led_fir = idf::GPIO_Output(idf::GPIONum(12));
-//auto led_sec = idf::GPIO_Output(idf::GPIONum(13));
-//static bool led_fir_status = false;
-//
-//ESPEventLoop eventLoop;
-//auto ledEvent = ESPEvent("led_event", ESPEventID(0xF1));
-//
-//auto reg = eventLoop.register_event_timed(
-//        ledEvent,
-//        [&led_sec](const ESPEvent &event, void *data) {
-//
-//            std::cout << "received event: " << event.base << "/" << event.id << std::endl;
-//            auto status = *reinterpret_cast<bool *>(data);
-//            if (status) {
-//                led_sec.set_low();
-//            } else {
-//                led_sec.set_high();
-//            }
-//            std::cout << "led event started" << std::endl;
-//        }, 500ms, [](const idf::event::ESPEvent &) {
-//            std::cout << "led event timeout..." << std::endl;
-//        }
-//);
-//
-//auto timer = std::make_unique<idf::esp_timer::ESPTimer>([&]() {
-//    if (led_fir_status) {
-//        led_fir.set_high();
-//    } else {
-//        led_fir.set_low();
-//    }
-//
-//    led_fir_status = !led_fir_status;
-//
-//    std::cout << "Test Timer Output" << std::endl;
-//
-//}, "TestTimer");
-//
-//// Start Timer  ms
-//timer->start_periodic(500ms);
-//
-//while (true) {
-//eventLoop.post_event_data(ledEvent, led_fir_status, 10ms);
-//std::cout << "Test out put" << std::endl;
-//std::this_thread::sleep_for(125ms);
-//}
-//}
 
+static const char *TAG = "Main";
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int event_id, void *event_data) {
+    if (event_base == WIFI_PROV_EVENT) {
+        switch (event_id) {
+            case WIFI_PROV_START:
+                ESP_LOGI(TAG, "Provisioning started");
+                break;
+            case WIFI_PROV_CRED_RECV: {
+                wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *) event_data;
+                ESP_LOGI(TAG, "Received Wi-Fi credentials"
+                              "\n\tSSID     : %s\n\tPassword : %s",
+                         (const char *) wifi_sta_cfg->ssid,
+                         (const char *) wifi_sta_cfg->password);
+                break;
+            }
+            case WIFI_PROV_CRED_FAIL: {
+                wifi_prov_sta_fail_reason_t *reason = (wifi_prov_sta_fail_reason_t *) event_data;
+                ESP_LOGE(TAG, "Provisioning failed!\n\tReason : %s"
+                              "\n\tPlease reset to factory and retry provisioning",
+                         (*reason == WIFI_PROV_STA_AUTH_ERROR) ?
+                         "Wi-Fi station authentication failed" : "Wi-Fi access-point not found");
+                break;
+            }
+            case WIFI_PROV_CRED_SUCCESS:
+                ESP_LOGI(TAG, "Provisioning successful");
+                break;
+            case WIFI_PROV_END:
+                /* De-initialize manager once provisioning is finished */
+                wifi_prov_mgr_deinit();
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 /**
  * @brief Application main
  * This Function Will Run With MainTask And When Called app_main() MainTask Ending;
  */
 extern "C" _Noreturn void app_main() {
+//    esp_err_t ret;
+
+    // Initialize NVS
+//    ret = nvs_flash_init();
+//    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+//        ESP_ERROR_CHECK(nvs_flash_erase());
+//        ret = nvs_flash_init();
+//    }
+//    ESP_ERROR_CHECK(ret);
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        /* NVS partition was truncated
+         * and needs to be erased */
+        ESP_ERROR_CHECK(nvs_flash_erase());
+
+        /* Retry nvs_flash_init */
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
+    /* Initialize TCP/IP */
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    esp_event_loop_create_default();
+
+    /* Initialize Wi-Fi including netif with default config */
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_prov_mgr_config_t config = {
+            .scheme = wifi_prov_scheme_ble,
+            .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM
+    };
+    wifi_prov_mgr_reset_provisioning();
+
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
+
+    const char *service_name = "WaterBox_BLU";
+    wifi_prov_security_t security = WIFI_PROV_SECURITY_0;
+    ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(security, nullptr, service_name, nullptr));
+
+    auto led = idf::GPIO_Output(idf::GPIONum(13));
+    auto led_status = false;
+    enum class Status {
+        Waiting,
+        Ending
+    };
+    auto now_status = Status::Waiting;
+    auto res = std::thread([&led, &now_status, &led_status]() -> void {
+        while (true) {
+            if (led_status) {
+                led.set_low();
+            } else {
+                led.set_high();
+            }
+            led_status = !led_status;
+
+            if (now_status == Status::Waiting) {
+                std::this_thread::sleep_for(200ms);
+            } else {
+                std::this_thread::sleep_for(1s);
+            }
+        }
+    });
+    wifi_prov_mgr_wait();
+
+    bool provisioned = false;
+    wifi_prov_mgr_is_provisioned(&provisioned);
+    if (provisioned) {
+        std::cout << "Prov Wifi Successful!" << std::endl;
+        wifi_prov_mgr_deinit();
+        esp_event_loop_delete_default();
+    } else {
+        std::cout << "Prov Wifi Error!" << std::endl;
+
+    }
+
     while (true) {
-        std::cout << "Test out put" << std::endl;
-        std::this_thread::sleep_for(125ms);
+        std::cout << "Tick Time" << std::endl;
+        std::this_thread::sleep_for(10s);
     }
 }
