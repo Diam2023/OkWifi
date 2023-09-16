baac1acd6758bd16488a60b94232729896bccb1a
monoliths <monoliths-uni@outlook.com>
2023-09-16 08:18:08 +0800

    Reset OkWifi class

--- main/OkWifi.cc
+++ main/OkWifi.cc
@@ -3,3 +3,19 @@
 //
 
 #include "OkWifi.h"
+
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include <memory>
+#include <cstring>
+#include "ProvServer.h"
+
+namespace ok_wifi {
+    using namespace std::chrono_literals;
+
+    static const char *TAG = "OkWifi";
+
+    OkWifi::OkWifi() {
+    }
+}
--- main/OkWifi.h
+++ main/OkWifi.h
@@ -5,19 +5,52 @@
 #ifndef OKWIFI_OKWIFI_H
 #define OKWIFI_OKWIFI_H
 
-/**
- * This Class Will Scan Ap If exist Prov Ap, this will connect it, And When Ap is not exist, it will call ble Prov
- * It Will steal scanning when ble prov is processing.
- */
-class OkWifi {
-private:
-
-public:
-    OkWifi();
+#include <thread>
+#include <mutex>
+#include <string>
+#include <vector>
+#include <memory>
+#include <esp_netif_types.h>
+#include "BleProv.h"
 
 
+namespace ok_wifi {
 
-};
+    /**
+     * This Class Will Scan Ap If exist Prov Ap, this will connect it, And When Ap is not exist, it will call ble Prov
+     * It Will steal scanning when ble prov is processing.
+     */
+    class OkWifi {
+    private:
 
+        // Ap station to completed
+        std::string service_ssid;
+        std::string ap_pwd = "OkWifi";
 
+        std::thread ok_wifi_thread;
+    public:
+
+        /**
+         * Create Thread to run it.
+         */
+        OkWifi();
+
+        /**
+         * Start Scan, if error it will call OkWifi And Run it.
+         */
+        void run();
+
+        /**
+         * Ap Mode to provide prov service
+         */
+        void prov_run();
+
+        static OkWifi& getInstance()
+        {
+            static OkWifi instance;
+            return instance;
+        };
+    };
+
+} // OKWIFI
 #endif //OKWIFI_OKWIFI_H
